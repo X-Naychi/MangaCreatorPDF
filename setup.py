7e07068e7d06f21e6a1e7af6106c3a6b5a7a12ee
X-Naychi <makarenko.itwork@gmail.com>
2021-12-07 19:03:48 +0200

    Added PDF converter and info messages

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-import os
-print('\n\n')
+import os 
+print('MangaCreatorPDF v1.1\n')
 
 part = 0
+sum_pages = 0
 
 def createDir(name):
     try: 
@@ -31,14 +32,17 @@ for item in list_dirs:
         continue
     elif 'Том ' + str(part) in item:
         len_files += len(files)
-        
+        sum_pages += len(files)
+
         i = 0
         while next_page <= len_files-1:
             try:
-                os.replace(item + '/' + str(i) + '.jpeg', name_title + '/' + partDir + '/' + str(next_page) + '.jpeg')
+                name_file = str(next_page) + '.jpeg'
+                os.replace(item + '/' + str(i) + '.jpeg', name_title + '/' + partDir + '/' + name_file.zfill(10))
             except FileNotFoundError:
                 if str(i)+'.png' in files:
-                    os.replace(item + '/' + str(i) + '.png', name_title + '/' + partDir + '/' + str(next_page) + '.png')
+                    name_file = str(next_page) + '.png'
+                    os.replace(item + '/' + str(i) + '.png', name_title + '/' + partDir + '/' + name_file.zfill(9))
                 else:
                     print('Файла "' + item+'/'+str(i)+'.png" - не существует!')
                     print('Аварийное завершение программы!')
@@ -57,9 +61,30 @@ for item in list_dirs:
         createDir(name_title + '/' + partDir)
         
         for f in files:
-            os.replace(item + '/' + f, name_title + '/' + partDir + '/' + f)
+            if not f.endswith(".png"):
+                os.replace(item + '/' + f, name_title + '/' + partDir + '/' + f.zfill(10))
+            else:
+                os.replace(item + '/' + f, name_title + '/' + partDir + '/' + f.zfill(9))
             next_page += 1
-    
+
     os.rmdir(item)
+
+print('Выполнена сортировка страниц и удалены ненужные папки.')
     
-    
+import img2pdf
+
+os.chdir(name_title)
+list_dirs = os.listdir()
+list_dirs.sort()
+
+for item in list_dirs:
+    files = os.listdir(item)
+    files.sort()
+
+    with open(item+".pdf", "wb") as f:
+        f.write(img2pdf.convert([item+'/'+i for i in files]))
+    print('Создан файл "' + item+ '.pdf"')
+
+print('\nКонвертирование манги в PDF завершено.')
+print('Отсортированные страницы и PDF файлы находяться по этому пути:\n"{}".\nВсего обработано {} тома(-ов) и {} страниц(-ы).\n\n'.format(os.getcwd(), part, sum_pages))
+input('Для завершения программы, нажмите любую клавишу...')
