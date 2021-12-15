c990b5f242b1689e81d95322e9f56f2542669705
X-Naychi <makarenko.itwork@gmail.com>
2021-12-15 14:21:44 +0200

    Changed the output of statistical data

--- MangaCreatorPDF.py
+++ MangaCreatorPDF.py
@@ -4,7 +4,7 @@ from colorama import init as colorInit, Style, Fore, Back
 
 colorInit()
 
-print(Style.BRIGHT + Fore.CYAN + 'MangaCreatorPDF v1.4.2-beta\nFor files with Mangal1b' + Style.RESET_ALL)
+print(Style.BRIGHT + Fore.CYAN + 'MangaCreatorPDF v1.4.3-beta\nFor files with Mangal1b' + Style.RESET_ALL)
 
 def askDir():
     print('\nВведи путь к каталогу:')
@@ -18,15 +18,6 @@ def askDir():
     else:
         return normpath(data)
 
-def askPart():
-    print('\nВведи c какого тома начинать делать мангу в PDF:')
-    try:
-        data = int(input())
-        return data
-    except ValueError:
-        print(Fore.RED + 'Введено не корректное значение. Пустота и буквы не проходят. \nПовтори ещё раз...\n' + Fore.RESET)
-        return askPart()  
-
 def askDeletedImages(dirs):
     user_answer = input('\nУдалить отсортированные страницы манги? [Y/n]: ')
     if user_answer in ['Y', 'y', 'Д', 'д']:
@@ -34,20 +25,10 @@ def askDeletedImages(dirs):
             shutil.rmtree(item, ignore_errors=True)
         print(Back.RED + Fore.BLACK + 'Удалено!' + Style.RESET_ALL)
     elif user_answer in ['N', 'n', 'Н', 'н']:
-        print('\nОтсортированные страницы и PDF файлы находяться по этому пути:\n"{}".\n'.format(Style.BRIGHT + Fore.YELLOW + os.getcwd() + Style.RESET_ALL))
+        print('\nОтсортированные страницы и PDF файлы находятся по этому пути:\n"{}".\n'.format(Style.BRIGHT + Fore.YELLOW + os.getcwd() + Style.RESET_ALL))
     else:
         print(Fore.RED + '\nВведён не корректный ответ. Повтори ещё раз... ' + Fore.RESET)
         askDeletedImages(dirs)
-        
-def askRepeat():
-    user_answer = input('\nБудем ещё какую-то мангу конвертировать? [Y/n]: ')
-    if user_answer in ['Y', 'y', 'Д', 'д']:
-        return True
-    elif user_answer in ['N', 'n', 'Н', 'н']:
-        return False
-    else:
-        print(Fore.RED + '\nВведён не корректный ответ. Повтори ещё раз... ' + Fore.RESET)
-        return askRepeat()
 
 def createDir(name):
     try: 
@@ -55,10 +36,10 @@ def createDir(name):
     except FileExistsError:
         pass
 
-while True:
-    # Counters
-    count_part, count_pages, count_del_png = 0, 0, 0
+statistic = {}
+repeat = True
 
+while repeat:
     os.chdir(askDir())
     list_dirs = [i for i in os.listdir() if os.path.isdir(i) and 'Глава' in i]
     list_dirs.sort(key=lambda x: int(x.split()[x.split().index('Том')+1]))
@@ -67,26 +48,28 @@ while True:
     name_title = os.path.basename(os.getcwd())
     createDir(name_title)
     os.chdir(name_title)
+    
+    statistic[name_title] = {'parts' : 0, 'pages' : 0, 'png' : 0}
 
     for item in list_dirs:
         current_part = item.split()[item.split().index('Том')+1]
         partDir = name_title + ' Том ' + current_part
         files = sorted(os.listdir('../' + item), key=lambda x: int(x.split('.')[0]))
         
-        if int(current_part) > count_part:
+        if int(current_part) > statistic[name_title]['parts']:
             createDir(normpath(partDir))
             num_page = 0 
-            count_part = int(current_part)
+            statistic[name_title]['parts'] = int(current_part)
         
         for page in files:
             new_name_file = str(num_page) + '.' + page.split('.')[1]
-            count_pages += 1                                            # Counter for statistic
+            statistic[name_title]['pages'] += 1
 
             if not page.endswith(".png"):                               # Move images
                 os.replace(normpath('../' + item + '/' + page), normpath(partDir + '/' + new_name_file))
             else:                                                       # DELETE PNG
                 os.remove(normpath('../' + item + '/' + page))
-                count_del_png += 1                                      # Counter for statistic
+                statistic[name_title]['png'] += 1
             num_page += 1
         
         os.rmdir('../' + item)
@@ -106,12 +89,22 @@ while True:
 
     askDeletedImages(list_dirs)
 
-    print('\n' + Back.GREEN + Fore.BLACK + 'Конвертирование "{}" в PDF завершено'.format(name_title) + Style.RESET_ALL)
-    print('Всего обработано {} тома(-ов) и {} страниц(-ы) из которых удалено {} PNG файла(-ов)'.format(count_part, count_pages, count_del_png))
+    print('\n' + Back.GREEN + Fore.BLACK + 'Конвертирование "{}" в PDF - завершено'.format(name_title) + Style.RESET_ALL)
+    
+    while True:
+        user_answer = input('\nБудем ещё какую-то мангу конвертировать? [Y/n]: ')
 
-    if askRepeat():
-        continue
-    else:
-        break
+        if user_answer in ['Y', 'y', 'Д', 'д']:
+            print('\n'.ljust(30, '='))
+            break
+        elif user_answer in ['N', 'n', 'Н', 'н']:
+            repeat = False
+            
+            print(Fore.YELLOW + '\nСтатистика:')
+            for title, values in statistic.items():
+                print(str('"'+title+'" ').ljust(45, '.') + ' Томов: {} | Страниц: {} | Удалено PNG: {}'.format(values['parts'], values['pages'], values['png']))
+            break
+        else:
+            print(Fore.RED + '\nВведён не корректный ответ. Повтори ещё раз... ' + Fore.RESET)
 
-input('\nДля завершения программы, нажмите Enter...')
+input(Style.RESET_ALL + '\nДля завершения программы, нажмите Enter...')
